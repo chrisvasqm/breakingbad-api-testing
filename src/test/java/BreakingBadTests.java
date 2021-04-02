92621e8eb850e8f078f1b240e69a4fd6710735dd
ISABELLA\Khrixuz <christianvasquezmelo@gmail.com>
2021-04-01 22:40:24 -0400

    Add print all character names and actors test

---
+++ src/test/java/BreakingBadTests.java
@@ -0,0 +1,42 @@
+import com.chrisvasqm.Character;
+import io.restassured.RestAssured;
+import io.restassured.mapper.ObjectMapperType;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
+
+public class BreakingBadTests {
+
+    @BeforeMethod
+    public void setUp() {
+        RestAssured.baseURI = "https://www.breakingbadapi.com";
+        RestAssured
+                .config()
+                .objectMapperConfig(objectMapperConfig().defaultObjectMapperType(ObjectMapperType.GSON));
+    }
+
+    @Test
+    public void testPrintAllCharacterNamesAndActors() {
+        final var characters = given()
+                .when()
+                .get("/api/characters")
+                .then()
+                .extract()
+                .body()
+                .jsonPath()
+                .getList(".", Character.class);
+
+        for (final var character : characters) {
+            System.out.println("The actor: " + character.getPortrayed() + " played the character: " + character.getName());
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        RestAssured.reset();
+    }
+
+}
