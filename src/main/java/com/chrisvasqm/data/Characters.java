6907c8a543fbeea05ffa19f3e3832979317ae205
chrisvasqm <christianvasquezmelo@gmail.com>
2021-10-12 20:46:57 -0400

    Move RestAssured setup

--- src/main/java/com/chrisvasqm/data/Characters.java
+++ src/main/java/com/chrisvasqm/data/Characters.java
@@ -1,12 +1,22 @@
 package com.chrisvasqm.data;
 
 import com.chrisvasqm.model.Character;
+import io.restassured.RestAssured;
+import io.restassured.mapper.ObjectMapperType;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
 
 public class Characters implements Repository<Character> {
+
+    public Characters() {
+        RestAssured.baseURI = "https://www.breakingbadapi.com/api";
+        RestAssured
+                .config()
+                .objectMapperConfig(objectMapperConfig().defaultObjectMapperType(ObjectMapperType.GSON));
+    }
 
     @Override
     public List<Character> getAll() {
--- src/test/java/BreakingBadTests.java
+++ src/test/java/BreakingBadTests.java
@@ -1,22 +1,14 @@
 import com.chrisvasqm.data.Characters;
 import io.restassured.RestAssured;
-import io.restassured.mapper.ObjectMapperType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
 
 public class BreakingBadTests {
     private Characters characters;
 
     @BeforeMethod
     public void setUp() {
-        RestAssured.baseURI = "https://www.breakingbadapi.com/api";
-        RestAssured
-                .config()
-                .objectMapperConfig(objectMapperConfig().defaultObjectMapperType(ObjectMapperType.GSON));
-
         characters = new Characters();
     }
 
